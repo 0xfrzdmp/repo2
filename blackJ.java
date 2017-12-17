1e79fc909e81247bcb988aca3858574102a4cf63
FreezeAutomation <derideriduke@yahoo.com>
2017-12-17 15:36:09 -0500

    modified blackTest

--- blackJ.java
+++ blackJ.java
@@ -4,6 +4,11 @@ public class blackJ{
  
       for (int i=1;i<=2;i++){
       System.out.println("Merry Christmas.."+i);
+      
+      }
+      
+      for(int i=1;i<=3;i++){
+      System.out.println("Happy New Year.."+i)
       }
  }
 
